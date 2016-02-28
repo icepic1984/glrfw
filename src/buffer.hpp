37db86535fdd78ff6f1895eab5c89851c8ce21a4
icepic <icepic2009@googlemail.com>
2016-02-28 18:24:59 +0100

    Add empty templates for buffer objects

---
+++ src/buffer.hpp
@@ -0,0 +1,16 @@
+#ifndef BUFFER_HPP
+#define BUFFER_HPP
+
+#include "handle.hpp"
+
+namespace glrfw {
+
+namespace detail {
+
+void delete_buffer(GLuint id);
+
+
+} // end namespace detail
+
+} // end namespace glrfw
+#endif 
